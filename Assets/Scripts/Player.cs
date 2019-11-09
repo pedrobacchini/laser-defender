649f293bab46b688462bb7fd808629ca85011aec
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-09 07:32:09 -0300

    Movement ship with unirx.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class Player : SerializedMonoBehaviour
@@ -9,60 +11,40 @@ public class Player : SerializedMonoBehaviour
 
     private float _xMin, _xMax, _yMin, _yMax;
 
-    public IObservable<Vector2> Movement { get; private set; }
-
     private void Start()
     {
         SetUpMoveBoundaries();
         // Movement inputs tick on FixedUpdate
-//        Movement = this.FixedUpdateAsObservable()
-//            .Select(_ => {
-//                var x = Input.GetAxis("Horizontal");
-//                var y = Input.GetAxis("Vertical");
-//                return new Vector2(x, y).normalized;
-//            });
+        this.FixedUpdateAsObservable()
+            .Select(_ => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")))
+            .Select(axis =>
+            {
+                var currentPosition = transform.position;
+                var newXPos = Mathf.Clamp(currentPosition.x + axis.x * Time.deltaTime * Speed, _xMin, _xMax);
+                var newYPos = Mathf.Clamp(currentPosition.y + axis.y * Time.deltaTime * Speed, _yMin, _yMax);
+                return new Vector2(newXPos, newYPos);
+            })
+            .Subscribe(newPosition => transform.position = newPosition);
     }
 
     private void SetUpMoveBoundaries()
     {
         var spriteRenderer = GetComponent<SpriteRenderer>();
-        if(spriteRenderer == null)
-            throw new ArgumentException("Missing Sprite Renderer "+ spriteRenderer.name);
+        if (spriteRenderer == null)
+            throw new ArgumentException("Missing Sprite Renderer " + spriteRenderer.name);
 
         var gameCamera = Camera.main;
         if (gameCamera == null)
             throw new ArgumentException("Put main camera in Scene");
-        
+
         var zeroPoint = gameCamera.ViewportToWorldPoint(new Vector2(0, 0));
         var onePoint = gameCamera.ViewportToWorldPoint(new Vector2(1, 1));
-            
+
         var size = spriteRenderer.size / 2;
-            
+
         _xMin = zeroPoint.x + size.x;
         _xMax = onePoint.x - size.x;
         _yMin = zeroPoint.y + size.y;
         _yMax = onePoint.y - size.y;
-
-        Debug.Log(_xMin + " " + _xMax);
-        Debug.Log(_yMin + " " + _yMax);
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-        var position = transform.position;
-
-        var deltaX = Input.GetAxis("Horizontal");
-        var deltaY = Input.GetAxis("Vertical");
-
-        var newXPos = Mathf.Clamp(position.x + deltaX * Time.deltaTime * Speed, _xMin, _xMax);
-        var newYPos = Mathf.Clamp(position.y + deltaY * Time.deltaTime * Speed, _yMin, _yMax);
-
-        transform.position = new Vector2(newXPos, newYPos);
     }
 }
