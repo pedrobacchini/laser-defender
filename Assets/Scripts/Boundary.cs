f4c3e2e9e2d4cb1bde210801076b8a224f17df18
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-16 17:54:44 -0300

    Coroutine Firing

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,15 @@
+﻿public class Boundary
+{
+    public float XMin { get; }
+    public float XMax { get; }
+    public float YMin { get; }
+    public float YMax { get; }
+
+    public Boundary(float xMin, float xMax, float yMin, float yMax)
+    {
+        XMin = xMin;
+        XMax = xMax;
+        YMin = yMin;
+        YMax = yMax;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UniRx;
@@ -10,29 +11,91 @@ public class Player : SerializedMonoBehaviour
     [OdinSerialize] private float Speed { get; set; } = 10;
     [OdinSerialize] private GameObject LaserPrefab { get; set; }
     [OdinSerialize] private float ProjectileSpeed { get; set; } = 20;
+    [OdinSerialize] private float ProjectileFiringPeriod { get; set; } = 0.1f;
 
-    private float _xMin, _xMax, _yMin, _yMax;
+    private Boundary _boundary;
 
     private void Start()
     {
         SetUpMoveBoundaries();
+
+        var axisObservable = this.FixedUpdateAsObservable()
+            .Select(_ => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+
         // Movement inputs tick on FixedUpdate
-        this.FixedUpdateAsObservable()
-            .Select(_ => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")))
+        axisObservable
             .Select(axis =>
             {
                 var currentPosition = transform.position;
-                var newXPos = Mathf.Clamp(currentPosition.x + axis.x * Time.deltaTime * Speed, _xMin, _xMax);
-                var newYPos = Mathf.Clamp(currentPosition.y + axis.y * Time.deltaTime * Speed, _yMin, _yMax);
+                var newXPos = Mathf.Clamp(currentPosition.x + axis.x * Time.deltaTime * Speed, _boundary.XMin,
+                    _boundary.XMax);
+                var newYPos = Mathf.Clamp(currentPosition.y + axis.y * Time.deltaTime * Speed, _boundary.YMin,
+                    _boundary.YMax);
                 return new Vector2(newXPos, newYPos);
             })
             .Subscribe(newPosition => transform.position = newPosition);
 
-        this.UpdateAsObservable()
-            .Where(_ => Input.GetButton("Fire1"))
-            .Select(_ => Instantiate(LaserPrefab, transform.position, Quaternion.identity))
-            .Select(laser => laser.GetComponent<Rigidbody2D>())
-            .Subscribe(rigidbody2dLaser => rigidbody2dLaser.velocity = new Vector2(0, ProjectileSpeed));
+        var isShooting = false;
+
+        axisObservable
+            .Select(axis => !axis.Equals(Vector2.zero))
+            .Subscribe(isMoving =>
+            {
+                if (!isMoving && !isShooting)
+                {
+                    _firingCoroutine = StartCoroutine(FireContinuously());
+                    isShooting = true;
+                }
+
+                if (isMoving && isShooting)
+                {
+                    StopCoroutine(_firingCoroutine);
+                    isShooting = false;
+                }
+            });
+
+//        this.UpdateAsObservable()
+//            .Where(_ => Input.GetButton("Fire1"))
+//            .Select(_ => Instantiate(LaserPrefab, transform.position, Quaternion.identity))
+//            .Select(laser => laser.GetComponent<Rigidbody2D>())
+//            .Select(rigidbody2dLaser => rigidbody2dLaser.velocity = new Vector2(0, ProjectileSpeed))
+//            .Subscribe();
+
+//            .TimeInterval(TimeSpan.FromSeconds(ProjectileFiringPeriod))
+//            .Delay(TimeSpan.FromSeconds(ProjectileFiringPeriod))
+//            .Subscribe();
+
+//        this.UpdateAsObservable()
+//            .Where(_ => Input.GetButtonDown("Fire1"))
+//            .Subscribe(_ => _firingCoroutine = StartCoroutine(FireContinuously()));
+//        
+//        this.UpdateAsObservable()
+//            .Where(_ => Input.GetButtonUp("Fire1"))
+//            .Subscribe(_ => StopCoroutine(_firingCoroutine));
+    }
+
+    private Coroutine _firingCoroutine;
+
+//    private void Update()
+//    {
+//        if (Input.GetButtonDown("Fire1"))
+//        {
+//            _firingCoroutine = StartCoroutine(FireContinuously());
+//        }
+//        if (Input.GetButtonUp("Fire1"))
+//        {
+//            StopCoroutine(_firingCoroutine);
+//        }
+//    }
+
+    private IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            var laser = Instantiate(LaserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ProjectileSpeed);
+            yield return new WaitForSeconds(ProjectileFiringPeriod);
+        }
     }
 
     private void SetUpMoveBoundaries()
@@ -50,9 +113,10 @@ public class Player : SerializedMonoBehaviour
 
         var size = spriteRenderer.size / 2;
 
-        _xMin = zeroPoint.x + size.x;
-        _xMax = onePoint.x - size.x;
-        _yMin = zeroPoint.y + size.y;
-        _yMax = onePoint.y - size.y;
+        _boundary = new Boundary(
+            zeroPoint.x + size.x,
+            onePoint.x - size.x,
+            zeroPoint.y + size.y,
+            onePoint.y - size.y);
     }
 }
