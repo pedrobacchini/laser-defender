4bdbec46326c5bab0243e4b3f5b392cc0cd8ab41
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-16 19:56:05 -0300

    Add EnemyPathing.

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+public class EnemyPathing : SerializedMonoBehaviour
+{
+    [OdinSerialize] private List<Transform> WayPoints { get; set; }
+}
