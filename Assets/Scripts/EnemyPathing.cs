784cb5149533af52c7c2be6cf1bb229864fe4394
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-16 21:26:33 -0300

    Enemy follow the waypoints.

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -1,9 +1,36 @@
 ﻿using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class EnemyPathing : SerializedMonoBehaviour
 {
     [OdinSerialize] private List<Transform> WayPoints { get; set; }
+
+    [OdinSerialize] private float MoveSpeed { get; set; } = 2f;
+
+    private int _wayPointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = WayPoints[0].position;
+
+        bool IsFinishMovement() => _wayPointIndex + 1 >= WayPoints.Count;
+
+        this.UpdateAsObservable()
+            .Where(_ => !IsFinishMovement())
+            .Subscribe(_ =>
+            {
+                var targetPosition = WayPoints[_wayPointIndex + 1].position;
+                var movementThisFrame = MoveSpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+                if (transform.position.Equals(WayPoints[_wayPointIndex + 1].position)) _wayPointIndex++;
+            });
+
+        this.UpdateAsObservable()
+            .Where(_ => IsFinishMovement())
+            .Subscribe(_ => Destroy(gameObject));
+    }
 }
