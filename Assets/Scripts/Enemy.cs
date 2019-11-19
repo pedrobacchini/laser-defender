8eec374fb56473cb4f8cddf74e6d4ade857848c6
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-19 20:09:54 -0300

    Make Enemy Shoot.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,46 @@
 ﻿using Sirenix.OdinInspector;
 using Sirenix.Serialization;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : SerializedMonoBehaviour
 {
     [OdinSerialize] private float Health { get; set; } = 100f;
+    [ReadOnly] [OdinSerialize] private float ShotCounter { get; set; }
+    [OdinSerialize] private float ProjectileSpeed { get; set; } = 20;
+    [MinMaxSlider(0, 5)] [OdinSerialize] private Vector2 TimeBetweenShoots { get; set; }
+    [OdinSerialize] private GameObject LaserPrefab { get; set; }
+
+    private void Start()
+    {
+        ResetShotCounter();
+    }
+
+    private void Update()
+    {
+        ShotCounter -= Time.deltaTime;
+        this.UpdateAsObservable()
+            .Where(_ => ShotCounter <= 0f)
+            .Subscribe(_ =>
+            {
+                Fire();
+                ResetShotCounter();
+            });
+    }
+
+    private void ResetShotCounter()
+    {
+        ShotCounter = Time.deltaTime + Random.Range(TimeBetweenShoots.x, TimeBetweenShoots.y);
+    }
+
+    private void Fire()
+    {
+        var position = transform.position;
+        var laser = Instantiate(LaserPrefab, new Vector2(position.x, position.y - 1), Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -ProjectileSpeed);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
