ebc0ce73b2d9066fafd9d2eca3ec55411b1a8362
Pedro Bacchini <pedrobacchini@outlook.com>
2019-11-24 17:25:26 -0300

    Sorting Layer & Spinning Projectile.

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Spinner : SerializedMonoBehaviour
+{
+    [OdinSerialize] private float SpinnerSpeed { get; set; } = 360f;
+
+    private void Start()
+    {
+        this.UpdateAsObservable()
+            .Subscribe(_ => transform.Rotate(new Vector3(0, 0 ,SpinnerSpeed * Time.deltaTime )));
+    }
+}
