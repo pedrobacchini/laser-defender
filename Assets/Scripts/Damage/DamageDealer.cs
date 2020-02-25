e158e01d54ca267647210aebe228540e125edd2c
Pedro Bacchini <pedrobacchini@outlook.com>
2020-02-25 13:48:00 -0300

    Add Impact Damage Dealer Effect

--- Assets/Scripts/Damage/DamageDealer.cs
+++ Assets/Scripts/Damage/DamageDealer.cs
@@ -1,13 +1,20 @@
 ﻿using Sirenix.OdinInspector;
 using Sirenix.Serialization;
+using UnityEngine;
 
 public class DamageDealer : SerializedMonoBehaviour
 {
     [OdinSerialize] public int Damage { get; private set; } = 100;
     [OdinSerialize] private bool isDestructible { get; set; } = true;
+    [OdinSerialize] private GameObject ImpactVFX = null;
 
     public void Hit()
     {
-        if(isDestructible) gameObject.SetActive(false);
+        if (ImpactVFX)
+        {
+            var transformPosition = transform.position;
+            Instantiate(ImpactVFX, new Vector2(transformPosition.x, transformPosition.y-0.5f), Quaternion.identity);   
+        }
+        if (isDestructible) gameObject.SetActive(false);
     }
 }
